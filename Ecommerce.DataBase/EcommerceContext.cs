4bdab235240d6d837785c4ea130f78e6ea59b6b5
KHAGESHOR GIRI <asndpmax@outlook.com>
2021-11-24 20:56:44 +0545

    Initial Project Setup

---
+++ Ecommerce.DataBase/EcommerceContext.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataBase
+{
+    public class EcommerceContext : DbContext
+    {
+        public EcommerceContext(): base("EcommerceConnection")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Ecommerce.DataBase/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Ecommerce.DataBase.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Ecommerce.DataBase.EcommerceContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Ecommerce.DataBase.EcommerceContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
