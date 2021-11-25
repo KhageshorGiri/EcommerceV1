269170241ea7658b6e780a0c7dbbcd094f13e032
KHAGESHOR GIRI <asndpmax@outlook.com>
2021-11-25 21:20:09 +0545

    Crud Operation for category

---
+++ Ecommerce.Services/CategoriesServices.cs
@@ -0,0 +1,53 @@
+﻿using Ecommerce.DataBase;
+using Ecommerce.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ecommerce.Services
+{
+    public class CategoriesServices
+    {
+        EcommerceContext db = new EcommerceContext();
+        public void saveCategory(Category category)
+        {
+            
+
+            //db.Categories.Add(category);
+
+            // or we can perform this action by following code also.
+            using (var context = new EcommerceContext())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+
+        public List<Category> getCategories()
+        {
+            return db.Categories.ToList();
+            
+        }
+
+        public Category getCatagory(int Id)
+        {
+            return db.Categories.Find(Id);
+        }
+
+        public void updateCategory(Category category)
+        {
+            db.Entry(category).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void deleteCategory(int Id)
+        {
+            var row = db.Categories.Find(Id);
+            db.Entry(row).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Ecommerce.Web/Controllers/CategoryController.cs
@@ -0,0 +1,63 @@
+﻿using Ecommerce.Entities;
+using Ecommerce.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesServices service = new CategoriesServices();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            var categories = service.getCategories();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            service.saveCategory(category);
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            var value = service.getCatagory(Id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            service.updateCategory(category);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int Id)
+        {
+            var category = service.getCatagory(Id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult ConformDelete(int Id)
+        {
+            service.deleteCategory(Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
