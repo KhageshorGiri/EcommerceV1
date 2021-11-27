34fc2b6f5fabda5af705375f8d319f7b89cc7eba
KHAGESHOR GIRI <asndpmax@outlook.com>
2021-11-27 16:15:05 +0545

    ajax crud operation in product

---
+++ Ecommerce.Services/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using Ecommerce.DataBase;
+using Ecommerce.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Services
+{
+    public class ProductService
+    {
+        EcommerceContext db = new EcommerceContext();
+
+        public void saveProduct(Product produt)
+        {
+            db.Products.Add(produt);
+            db.SaveChanges();
+        }
+
+        public List<Product> getProduct()
+        {
+            return db.Products.ToList();
+        }
+
+        public Product getProduct(int Id)
+        {
+            return db.Products.Find(Id);
+        }
+
+        public void updateProduct(Product product)
+        {
+            db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void Delete(int Id)
+        {
+            var row = db.Products.Find(Id);
+            db.Entry(row).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Ecommerce.Web/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Ecommerce.Entities;
+using Ecommerce.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductService service = new ProductService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductTable(string search)
+        {
+            var products = service.getProduct();
+
+            if (search != null)  //string.IsNullOrEmpty(search) == false
+            {
+                products = products.Where(x => x.Name != null && x.Name.ToLower().Contains(search.ToLower())).ToList();
+            }
+
+            return PartialView(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Create(Product product)
+        {
+            service.saveProduct(product);
+            return Redirect("Index");
+        }
+    }
+}
